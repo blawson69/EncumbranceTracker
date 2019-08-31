0e1c09fa158eb38bedb41cb7e8b7cd7c49dee264
Ben L <39037350+blawson69@users.noreply.github.com>
2019-08-30 19:20:47 -0700

    Update EncumbranceTracker.js
    Corrected Venmo link in README

--- EncumbranceTracker.js
+++ EncumbranceTracker.js
@@ -4,7 +4,7 @@ Monitors a character's Encumbrance levels on the Roll20 5e Shaped Sheet.
 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
-Like this script? Buy me a coffee: https://venmo.com/theBenLawson
+Like this script? Buy me a coffee: https://venmo.com/theRealBenLawson
 */
 
 var EncumbranceTracker = EncumbranceTracker || (function () {
